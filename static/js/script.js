0092d86b06bfb33f00bec426cd2e597d9a9e2d68
“Otgonbayar <btgonbayr@gmail.com>
2025-03-20 18:23:09 +0000

    Introduced footer, improved appearance of profile and recipe pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Function for sidebar menu toggle
     let toggleButton = document.querySelector(".toggle"); 
-    let icon = toggleButton.querySelector("i");
     const sidebar = document.querySelector(".sidebar");
+    const homepageURLs = ["/", "/dishcovery/"];
+
+    if (homepageURLs.includes(window.location.pathname)) {
+        sidebar.classList.add("sidebar-active");
+        toggleButton.classList.add("toggle-active");
+    }
 
     toggleButton.onclick = function () {
-        if (icon.classList.contains("fa-bars")) {
-            icon.classList.replace("fa-bars", "fa-times");
-        } else {
-            icon.classList.replace("fa-times", "fa-bars");
-        }
-    };
-
-    toggleButton.addEventListener("click", function () {
         sidebar.classList.toggle("sidebar-active");
         toggleButton.classList.toggle("toggle-active");
-    });
+    };
 });
